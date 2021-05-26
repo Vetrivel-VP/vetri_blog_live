829c410a4409dbf9e8146bf721ac2368d996cd3f
VETRIVEL <vetrivel.galaxy@gmail.com>
2021-05-26 20:08:45 +0300

    First push : To deploy app in heroku

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
-const API_URL = "http://localhost:3000/api/posts";
-const API_BASE_URL = "http://localhost:3000/";
+const API_URL = "https://vetriblogapi.herokuapp.com/api/posts";
+const API_BASE_URL = "https://vetriblogapi.herokuapp.com/";
 
 window.onload = () => {
   getPosts();
